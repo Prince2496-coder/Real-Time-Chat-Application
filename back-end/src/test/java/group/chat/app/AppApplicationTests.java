a4ed51dd0d00c34ac62b5b3b4ba08012599ea85c
Prince <p33605855@gmail.com>
2026-06-24 16:10:08 +0530

    Initial commit - Chat Application

---
+++ back-end/src/test/java/group/chat/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package group.chat.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
